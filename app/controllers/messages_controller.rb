e8fc6c02303de6eb782f4c2e260ac11ed90f68fd
rangubell <rangubell@icloud.com>
2023-05-26 14:25:46 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
     @message = Message.new
     @room = Room.find(params[:room_id])
   end
+
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
